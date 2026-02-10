445617c80f8450bb92de287424abf1aa89cea132
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2026-02-10 14:49:41 -0400

    Adding custom UserDetailsManager from mysql db

---
+++ spring-boot4-security-2/README.md
@@ -0,0 +1,9 @@
+# Spring Security with mysql db
+
+## Branches with different features
+
+- #### branch: part07_spring_security_with_jdbc
+JdbcUserDetailsManager & PasswordEncoder
+
+- #### branch: part08_custom_userDetailsManager_custom_user_from_mysql
+Custom UserDetailsManager & Custom User from MySql
--- spring-boot4-security-2/src/main/resources/data.sql
+++ spring-boot4-security-2/src/main/resources/data.sql
@@ -1,10 +1,11 @@
+-- it is for PasswordEncoder = new BCryptPasswordEncoder();
 -- INSERT IGNORE INTO users (username, password, enabled) VALUES
 --    ('user', '$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', '1'),
 --    ('admin', '$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', '1');
 
-INSERT IGNORE INTO users (username, password, enabled) VALUES
-    ('user', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', '1'),
-    ('admin', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', '1');
+INSERT IGNORE INTO users (username, name, email, password, enabled) VALUES
+    ('user', 'Willy De Keyser', 'user@fake.com', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', '1'),
+    ('admin', 'Tom Black', 'admin@fake.com', '{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW', '1');
 
 INSERT IGNORE INTO authorities (username, authority) VALUES
     ('user', 'ROLE_USER'),
