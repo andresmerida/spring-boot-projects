b94d28578b3c9256404a93893fa62e7705c9c801
Andres Merida <andresmerida@Andress-MacBook-Pro.local>
2026-02-16 15:25:56 -0400

    Adding initial commit for spring security json web token

---
+++ spring-security-jwt/src/main/java/dev/am/ss_jwt/SpringSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package dev.am.ss_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpringSecurityJwtApplication.class, args);
+	}
+
+}
---
+++ spring-security-jwt/src/main/java/dev/am/ss_jwt/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package dev.am.ss_jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .formLogin(login -> login
+                        .defaultSuccessUrl("/", true)
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutSuccessUrl("/")
+                        .deleteCookies("JSESSIONID")
+                        .permitAll())
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/", "/public").permitAll()
+                        .requestMatchers("/user/**").hasRole("USER")
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
